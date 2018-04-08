11e46b88b7154cf3f2e6909a72fe97546cdfec8b
lufengren <35322784+lufengren@users.noreply.github.com>
2018-04-07 20:02:49 -0700

    updatetime

--- python_stack/flask_fundamentals/ninjagold/server.py
+++ python_stack/flask_fundamentals/ninjagold/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect
 import random
+import datetime
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 @app.route('/')
@@ -12,29 +13,29 @@ def index():
 
 @app.route('/process_money',methods=['POST'])
 def process_money():
-    
+    currenttime = datetime.datetime.now()
     if request.form['building']=='farm':
         randnum = random.randrange(10,21)
         session['goldsnumber'] += randnum
-        session['activity'].append("Earned {} golds from the farm!".format(randnum))
+        session['activity'].append("Earned {} golds from the farm! {}".format(randnum,currenttime))
     elif request.form['building']=='cave':
         randnum = random.randrange(5,11)
         session['goldsnumber'] += randnum
-        session['activity'].append("Earned {} golds from the cave!".format(randnum))
+        session['activity'].append("Earned {} golds from the cave! {}".format(randnum,currenttime))
     elif request.form['building']=='house':
         randnum = random.randrange(2,6)
         session['goldsnumber'] += randnum
-        session['activity'].append("Earned {} golds from the house!".format(randnum))
+        session['activity'].append("Earned {} golds from the house! {}".format(randnum,currenttime))
     elif request.form['building']=='casino':
         addortake = random.randrange(0,2)
         if addortake==0:
             randnum = random.randrange(0,51)
             session['goldsnumber'] += randnum
-            session['activity'].append("Entered a casino and earned {} golds".format(randnum))
+            session['activity'].append("Entered a casino and earned {} golds {}".format(randnum,currenttime))
         else:
             randnum = random.randrange(0,51)
             session['goldsnumber'] -= randnum
-            session['activity'].append("Entered a casino and lost {} golds...Ouch....".format(randnum))
+            session['activity'].append("Entered a casino and lost {} golds...Ouch.... {}".format(randnum,currenttime))
     return redirect("/")
 
 
