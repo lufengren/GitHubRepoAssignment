5103472f9b2830c8a437adbc56a3328f71664691
lufengren <35322784+lufengren@users.noreply.github.com>
2018-04-17 16:20:44 -0700

    dojoninjas

---
+++ python_stack/django/users/apps/dojoninja/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name=models.CharField(max_length=255)
+    city=models.CharField(max_length=255)
+    state=models.CharField(max_length=255)
+    desc=models.TextField(default="")
+class Ninja(models.Model):
+    dojo_id=models.ForeignKey(Dojo, related_name="dojos")
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    
