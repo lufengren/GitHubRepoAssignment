fce7dbf539a4e524334a036d6ef73436212783d6
lufengren <35322784+lufengren@users.noreply.github.com>
2018-05-02 18:29:01 -0700

    carsandcats_ejs

--- mean/express/carsandcats/server.js
+++ mean/express/carsandcats/server.js
@@ -3,9 +3,17 @@ var express = require("express");
 var app = express();
 console.log("Let's find out what app is", app);
 app.use(express.static(__dirname + "/static"));
-console.log(__dirname);
-// app.set('views', __dirname + '/views'); 
-// app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+app.get("/cars", function (request, response){
+    response.render('cars');
+});
+app.get("/cats", function (request, response){
+    response.render('cats');
+});
+app.get("/cars/new", function (request, response){
+    response.render('newcars');
+});
 
 app.listen(8001, function () {
     console.log("listening on port 8001");
