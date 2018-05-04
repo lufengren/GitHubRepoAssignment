ba02748f3ae14e5344e17d5920077c53aef2331d
lufengren <35322784+lufengren@users.noreply.github.com>
2018-05-03 22:32:45 -0700

    epic_button

---
+++ mean/express/epic_button/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+//var bodyParser = require('body-parser');
+const app = express();
+var server = require('http').createServer(app);
+const io = require('socket.io')(server);
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+app.use(express.static(__dirname + "/public"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+server.listen(1337);
+
+app.get("/", function (request, response) {
+    response.render('index');
+});
+var count=0;
+io.on('connection', function (socket) {
+    socket.on("click", function (data) {
+        console.log(data.msg);
+        count++;
+        console.log(count);
+        io.emit("count", {data:count});
+        
+    });
+});
+
+
+
+
+
