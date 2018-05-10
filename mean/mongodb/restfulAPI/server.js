3951590bb1f2be3611a020aa9b4d1b23a93213ba
lufengren <35322784+lufengren@users.noreply.github.com>
2018-05-09 22:15:28 -0700

    add_angular

--- mean/mongodb/restfulAPI/server.js
+++ mean/mongodb/restfulAPI/server.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyparser = require('body-parser');
+
 const app=express();
 app.use(bodyparser.json());
+app.use(express.static(__dirname + '/restfulangular/dist/restfulangular' ));
 mongoose.connect('mongodb://localhost/Tasks');
 
 const Taskschema=new mongoose.Schema({
