3c97f14b3230385815fdb53fdc6486f8e06f486e
lufengren <35322784+lufengren@users.noreply.github.com>
2018-06-09 20:45:17 -0700

    survey_model

---
+++ C#/Survey_model/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Survey_model.Models;
+
+namespace Survey_model.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("result")]
+        public IActionResult result(Ninja newninja)
+        {
+            return View(newninja);
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#/Survey_model/Models/Ninja.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Survey_model.Models
+{
+    public class Ninja
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Language { get; set; }
+        public string Comment { get; set; }
+    }
+}
